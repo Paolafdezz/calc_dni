388be555e3acd3d35a996b6246458eb602d38b5c
DESKTOP-16N6QCF\Paola <paola>
2023-05-30 09:08:55 +0200

    calculadora dni

---
+++ main.py
@@ -0,0 +1,21 @@
+from calculadora_dni import calculadora_dni
+
+def pideUnNumMax8Digitos():
+    msg = "Dame un num: "
+    entrada = ""
+    invalido = False
+    numero = -1
+    while numero >= 99999999 or numero < 0:
+        while not entrada.isnumeric():
+            if invalido:
+                print ("Obligatoriamente debe de ser un numero")
+            invalido = True
+
+            entrada = input(msg)
+        numero = int(entrada)
+
+    return numero
+
+numero_dni = pideUnNumMax8Digitos()
+letra_dni = calculadora_dni(numero_dni)
+print(f"DNI CALCULADO: {numero_dni}-{letra_dni}")
